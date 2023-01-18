c47d71cef085a104ac5a81300d27f4402f78ca91
Neyakira <me@nevysha.art>
2023-01-18 14:30:31 +0100

    parameters file

--- preferences.exemple.js
+++ preferences.exemple.js
@@ -10,7 +10,9 @@ const preferences = {
 
 const preferences = {
     foldersPath: [
-
+        "D:\\stable-diffusion\\A1111 Web UI Autoinstaller\\stable-diffusion-webui\\outputs\\txt2img-images",
+        "D:\\stable-diffusion\\A1111 Web UI Autoinstaller\\stable-diffusion-webui\\outputs\\img2img-images",
+        "D:\\stable-diffusion\\A1111 Web UI Autoinstaller\\stable-diffusion-webui\\outputs\\saves",
     ]
 }
 
