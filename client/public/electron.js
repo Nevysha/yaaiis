12c2893c3b6ab523fa035828c5b3e3f2bb55f133
Neyakira <me@nevysha.art>
2023-01-18 14:20:34 +0100

    integration of Yaaiis and automatic1111 in electron app

--- client/public/electron.js
+++ client/public/electron.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow, BrowserView } = electron;
 const path = require('path');
 const isDev = require('electron-is-dev');
 
-let mainWindow = null;
+let mainWindow;
 app.on('ready', createWindow);
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
@@ -15,23 +15,71 @@ app.on('activate', function () {
         createWindow()
     }
 });
+
+const _RATIO = (3 / 5);
+function calcYaaiis() {
+    const contentBounds = mainWindow.getContentBounds();
+    return {width:Math.round(contentBounds.width * _RATIO) - 1,height:contentBounds.height};
+}
+function calcAutomatic1111() {
+    const contentBounds = mainWindow.getContentBounds();
+    return {width:Math.round(contentBounds.width * (1 - _RATIO)) - 1,height:contentBounds.height};
+}
+
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 1024,
-        title: "Yaaiis"
+        width: 2420,
+        height: 1080,
     });
 
-    const view = new BrowserView()
-    mainWindow.setBrowserView(view)
-    view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
-    view.webContents.loadURL('https://electronjs.org')
-
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-    mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+    mainWindow.loadFile(`electron-index.html`);
     mainWindow.on('page-title-updated', function (e) {
         e.preventDefault()
+
+        const viewYaaiis = new BrowserView()
+        const viewAutomatic1111 = new BrowserView();
+
+        function setYaaiisBounds() {
+            viewYaaiis.setBounds({
+                x: 0,
+                y: 0,
+                width: calcYaaiis().width,
+                height: calcYaaiis().height
+            });
+        }
+
+        function setAutomatic1111Bounds() {
+            viewAutomatic1111.setBounds({
+                x: calcYaaiis().width,
+                y: 0,
+                width: calcAutomatic1111().width,
+                height: calcYaaiis().height
+            });
+        }
+
+        mainWindow.addBrowserView(viewYaaiis);
+        mainWindow.addBrowserView(viewAutomatic1111);
+
+        setYaaiisBounds();
+        setAutomatic1111Bounds();
+
+        mainWindow.on('resize', () => {
+            const contentBounds = mainWindow.getContentBounds();
+            console.log(contentBounds)
+            setYaaiisBounds();
+            setAutomatic1111Bounds();
+        })
+
+        viewYaaiis.webContents.loadURL('http://localhost:3000');
+        viewAutomatic1111.webContents.loadURL('http://localhost:7860');
+
+        mainWindow.on('closed', function () {
+            mainWindow = null
+        })
+        mainWindow.on('page-title-updated', function (e) {
+            e.preventDefault()
+        });
     });
+
+
 }
